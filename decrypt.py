7f42a43d41b6ae77eb3eee89761143eb0b696c45
Echo <66488397+NullByte75@users.noreply.github.com>
2021-07-17 20:06:44 +0200

    Update decrypt.py

--- decrypt.py
+++ decrypt.py
@@ -17,23 +17,58 @@ def main():
                           <|i::|i|>                     
 \u001b[38;5;220m                           |[::|.|                      
                             ||: |                       \u001b[0m
-\u001b[41m───────────────────────────BRAINZDECRYPT───────────────────────────\u001b[0m
+\u001b[41m───────────────────────────BRAINZCRYPT───────────────────────────\u001b[0m
 """
     print(banner)
     print(Fore.LIGHTRED_EX)
-    print("Enter file to decrypt: ")
+    print("Do you want to decrypt a folder or a file?\n1: File\n2: Folder")
     print(Fore.YELLOW)
-    filename = input(str(">>> "))
-    with open('key.key', 'r') as file:
-        encoded = file.readline()
-        f = Fernet(encoded)
-    with open(filename, "rb") as file:
-            file_data = file.read()
-            encrypted_data = f.decrypt(file_data)
-    with open(filename, "wb") as file:
-        file.write(encrypted_data)
-        file.close()
-    print("File decrypted successfully!")
+    choic = input(str(">>> "))
+    if choic == "1":
+        print(Fore.LIGHTRED_EX)
+        print("Enter full path of file to decrypt: ")
+        print(Fore.YELLOW)
+        filename = input(str(">>> "))
+        with open('key.key', 'r') as file:
+            encoded = file.readline()
+            f = Fernet(encoded)
+        with open(filename, "rb") as file:
+                file_data = file.read()
+                decrypted_data = f.decrypt(file_data)
+        with open(filename, "wb") as file:
+            file.write(decrypted_data)
+            file.close()
+        print("File decrypted successfully!")
+        input("Press enter to continue")
+    if choic == "2":
+        print(Fore.LIGHTRED_EX)
+        print("Enter full path of folder to decrypt: ")
+        print(Fore.YELLOW)
+        dirname = input(str(">>> "))
+        list = os.listdir(dirname)
+        for file1 in list:
+            file1 = dirname + "/" + file1
+            with open('key.key', 'r') as file:
+                encoded = file.readline()
+                f = Fernet(encoded)
+            with open(file1, "rb") as file:
+                    file_data = file.read() 
+                    decrypted_data = f.decrypt(file_data)
+            with open(file1, "wb") as file:
+                file.write(decrypted_data)
+                file.close()
+            print("Derypted file " + file1)
+        print("\nFolder decrypted successfully!")
+        input("Press enter to continue")
+
+    uname = os.uname()
+    if uname.sysname == "Linux":
+        os.system("clear")
+        main()
+    if uname.sysname == "Windows":
+        os.system("cls")
+        main()
+        
 try:
     import random
     import os
@@ -42,5 +77,5 @@ try:
     from colorama import Fore, Back
     main()
 except Exception as e:
-    print("Unable to start BrainZDecrypt due to:")
+    print("Unable to start BrainZCrypt due to:")
     print(e)
